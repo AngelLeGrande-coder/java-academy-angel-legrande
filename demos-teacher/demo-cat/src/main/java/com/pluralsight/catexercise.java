7489015efc23ac24829a238de3c8b9b1e663485f
Angel LeGrande <angellegrande14209@gmail.com>
2025-09-24 23:16:03 -0700

    Finished Exercise

---
+++ demos-teacher/demo-cat/src/main/java/com/pluralsight/catexercise.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class catexercise {
+
+    public static void main(String[] args) {
+        // Variables about a cat
+        String catName = "Demon"; // text
+        int catAge = 1; // whole number
+        double catWeight = 1.3; // decimal number
+        char catGrade = 'A'; // single character
+        boolean isHungry = true; // true/false
+        boolean isBegging = false;
+        System.out.println("Meet Demon !");
+        System.out.println("Age = 1");
+        System.out.println("Weight = 1.3kg");
+        System.out.println("isHungry = true");
+    }
+}
