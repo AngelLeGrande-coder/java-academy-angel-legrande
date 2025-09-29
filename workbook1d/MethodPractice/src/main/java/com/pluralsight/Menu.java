8f26451cc61f721f11f6e96dfc8ac28fdbee5aea
Angel LeGrande <angellegrande14209@gmail.com>
2025-09-29 02:18:26 -0700

    method practice

---
+++ workbook1d/MethodPractice/src/main/java/com/pluralsight/Menu.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Menu {
+    public static void main(String[] args) {
+        displayMenu();
+    }
+    public static void displayMenu(){
+        System.out.println("=== MENU ===" );
+        System.out.println("1. Black Coffee - $2.25");
+        System.out.println("2. Green Tea - $3.50");
+        System.out.println("3. Raisin Cookie - $2.50");
+    }
+}
