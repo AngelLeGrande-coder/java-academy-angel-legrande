861610b6b9b0a90b31e751a25cdded0afc0c89da
Angel LeGrande <angellegrande14209@gmail.com>
2025-09-29 13:10:28 -0700

    Financial Calculator

---
+++ workshops/financial-calculators/src/main/java/com/pluralsight/FinancialCalculator.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FinancialCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Hello, welcome to our precise mortgage calculator!");
+
+
+    }
+}
