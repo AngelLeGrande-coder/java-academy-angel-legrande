1fd0daedab2484de54c2aaad1b9658435796611d
Angel LeGrande <angellegrande14209@gmail.com>
2025-09-29 01:40:40 -0700

    Math App

--- workbook1d/MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ workbook1d/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -1,52 +1,66 @@
 package com.pluralsight;
 
-//exercise 2
 public class MathApp {
     public static void main(String[] args) {
-        //question 1 - salaries, find max
-        double bobSalary = 150000.00;
-        double garySalary = 100000.50;
-        double highestSalary = Math.max(bobSalary, garySalary);
-        System.out.println("1. The highest salary is " + highestSalary);
+        //Question 1:
+        // declare variables here
+        // then code solution
+        double bobSalary = 12345;
+        double garySalary = 12345.1;
+        double highestSalary = Math.max(bobSalary,garySalary);
 
-        //question 2 - automotives, find min
-        double carPrice = 25500.25;
-        double truckPrice = 46000.60;
-        double lowestPrice = Math.min(carPrice, truckPrice);
-        System.out.println("2. The lowest automotive price is " + lowestPrice);
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
 
-        //question 3 - area of circle
-        double radius = 3.5;
-        double area = Math.PI * Math.pow(radius, 2);
-        System.out.println("3. The area of a circle with radius " + radius + " is " + area + ".");
+        System.out.println("The highest salary is: " + highestSalary);
 
-        //question 4 - sqrt
-        double myNum = 5.0;
-        System.out.println("4. The square root of 5 is " + Math.sqrt(myNum));
+        //Q2
+        double carPrice = 50_000;
+        double truckPrice = 55_000;
+        double cheapestVehicle = Math.min(carPrice, truckPrice);
 
-        //question 5 - distance b/w 2 points
-        int x1 = 5;
-        int y1 = 10;
-        int x2 = 85;
-        int y2 = 50;
+        System.out.println(cheapestVehicle);
+
+        //Q3
+        double radius = 7.25;
+        double circleArea = Math.PI * Math.pow(radius, 2);
+
+        System.out.println("The area of a circle with " + radius + " is: " + circleArea);
+
+        //Q4
+        double num = 5.0;
+        double numSquareRooted = Math.sqrt(num);
+
+        System.out.println(numSquareRooted);
+
+        //Q5
+        int x1 = 5, y1 = 10;
+        int x2 = 85, y2 = 50;
+        //distance = sqrt((x2-x1)^2 + (y2-y1)^2)
         double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.printf("5. The distance between (5, 10) and (85, 50) is %.3f.\n", distance);
+        System.out.println("The distance between (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ") is: " + distance);
 
-        //question 6 - abs val
-        double negDouble = -3.8;
-        System.out.println("6. The absolute value of " + negDouble + " is " + Math.abs(negDouble) + ".");
+        //Q6
+        float num1 = -3.8f;
+        float num2 = Math.abs(num1);
 
-        //question 7 - random num
+        System.out.println("the absolute value of -3.8 is " + num2 );
+
+        //Q7
         double randomNum = Math.random();
-        System.out.println("7. The randomly generated number is... " + randomNum + "!");
+        System.out.println(randomNum);
 
-        //question 8 - minutes in days
-        final int HOUR_IN_DAY = 24, MIN_IN_HOUR = 60, SEC_IN_MIN = 60, MS_IN_SEC = 1000;
-        int numDays = 24;
-        int numMinutes = numDays * HOUR_IN_DAY * MIN_IN_HOUR;
-        int numMs = numMinutes * SEC_IN_MIN * MS_IN_SEC;
-        System.out.println("8. In 24 days, there are " + numMinutes + " minutes "
-                + "or " + numMs + " milliseconds.");
+        //Q8
+        int days = 24;
+        int hoursInDay = 24;
+        int minsInHour = 60;
+        int secondsInMin = 60;
+        int millisecondsInSeconds = 1000;
 
+        int totalMinutes = days * hoursInDay * minsInHour;
+        System.out.println("Total minutes in 24 days: " + totalMinutes);
+
+        long totalMilliseconds = days * hoursInDay * minsInHour * secondsInMin * millisecondsInSeconds;
+        System.out.println("Total milliseconds in 24 days: " + totalMilliseconds);
     }
 }
